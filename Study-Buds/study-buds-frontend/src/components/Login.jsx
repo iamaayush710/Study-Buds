7f3ddef9b2f3a6b0eaf0476c3f10ecd2d7b64af1
Paige <pscott10@murraystate.edu>
2024-12-05 21:47:46 -0600

    Changed Login/Registration Design

--- Study-Buds/study-buds-frontend/src/components/Login.jsx
+++ Study-Buds/study-buds-frontend/src/components/Login.jsx
@@ -1,8 +1,5 @@
 import React, { useState } from 'react';
 import { TextField, Button, Typography, Box } from '@mui/material';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import GoogleIcon from '@mui/icons-material/Google';
-import TwitterIcon from '@mui/icons-material/Twitter';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import './AuthForm.css';
@@ -21,18 +18,16 @@ const Login = () => {
 
     try {
       const response = await axios.post('http://localhost:5001/auth/login', { email, password });
-
-      // Assuming the backend sends a token upon successful login
       const { token } = response.data;
 
-      // Store the token in localStorage for future authenticated requests
+      //store token for future use
       localStorage.setItem('token', token);
 
-      // Display success message and redirect to dashboard
+  
       alert(`Login successful: ${response.data.message}`);
       navigate('/dashboard'); // Redirect to dashboard page
+
     } catch (err) {
-      // Set error message from backend or fallback message
       setError(err.response?.data?.error || 'Login failed. Please try again.');
     } finally {
       setLoading(false);
@@ -42,6 +37,7 @@ const Login = () => {
   return (
     <Box className="auth-container">
       <Box className="auth-card">
+        <Typography className="app-name">StudyBuds</Typography>
         <Typography variant="h4" gutterBottom>
           Login
         </Typography>
@@ -73,23 +69,10 @@ const Login = () => {
               {error}
             </Typography>
           )}
-          <Box display="flex" justifyContent="space-between" marginBottom={2}>
-            <Button color="primary" variant="text" className="forgot-password-btn">
-              Forgot password?
-            </Button>
-          </Box>
           <Button variant="contained" color="primary" fullWidth type="submit" disabled={loading}>
             {loading ? 'Logging in...' : 'LOGIN'}
           </Button>
         </form>
-        <Typography variant="body1" align="center" marginTop={2}>
-          Or Sign Up Using
-        </Typography>
-        <Box display="flex" justifyContent="center" marginTop={2}>
-          <Button startIcon={<FacebookIcon />} color="primary" />
-          <Button startIcon={<GoogleIcon />} color="error" />
-          <Button startIcon={<TwitterIcon />} color="info" />
-        </Box>
         <Typography variant="body2" align="center" marginTop={2}>
           Don't have an account? <a href="/register">SIGN UP</a>
         </Typography>
